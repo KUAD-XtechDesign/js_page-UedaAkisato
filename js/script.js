b790ccdf21a4d74f0d2ad4f7d39e0e6d48d434f9
UedaAkisato <a-ueda7273@st.kyoto-art.ac.jp>
2021-11-11 04:06:14 +0900

    変更１

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 $(function(){
-
   
-})
+    $("#slide").slick({
+        dots:true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 200,
+    });
+    
+    // $(".btn").on("click",()=>{
+    //   $("#slide").slideToggle();
+    // })
+  
+  });
